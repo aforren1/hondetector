db527d0366730f7f32fd501a5ac57c9eb8e8858e
Alex Forrence <aforren1@jhu.edu>
2017-02-05 10:48:57 -0500

    fix startup script

--- bash/on_startup.sh
+++ bash/on_startup.sh
@@ -5,4 +5,6 @@ if [ "$?" != 0 ]; then
   echo "No internet!"
   sleep 20
 fi
-sudo python /home/pi/Desktop/detect_hon.py
+cd /home/pi/Desktop/hondetector
+source venv/bin/activate
+sudo python detect_hon.py
