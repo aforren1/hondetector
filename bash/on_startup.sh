e19b1b76560eabd4b0ee24a68a8f101a812a74d0
Alex Forrence <aforren1@jhu.edu>
2017-03-04 18:06:36 -0500

    Update on_startup.sh
    try to force wifi connection (untested)

--- bash/on_startup.sh
+++ bash/on_startup.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 sleep 15
+# setup wifi -- initial command is `wpa_passphrase Your_SSID Your_passwd`
+ip link set wlan0 down
+ip link set wlan0 up
+wpa_supplicant -B -iwlan0 -c/etc/wpa_supplicant.conf -Dwext
+sudo dhclient wlan0
+# check if we have internet
 wget --spider www.google.com
 if [ "$?" != 0 ]; then
   echo "No internet!"
